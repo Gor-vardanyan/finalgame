1b2de3ad3d4fd39c18dacece559a73cf9c13131e
Gor <g.var.design@gmail.com>
2020-12-31 20:45:18 +0100

    show players for user

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.post('/login', logUser); //login //+
 app.post('/logoutuser', auth, LogoutUser); //logout //+
 app.post('/profile', auth, profile); // shows the profile of the acount loged //+
 app.post('/delete', auth, deleteUser); //deletes loged user //+
+app.post('/player/show',auth, showPlayers); //show all players, only for admin //+
+
 //ADMIN
 app.post('/admin/user/show', auth, chekrole, showUser); //show all users, only for admin //+
 app.post('/admin/user/delete', auth, chekrole, deleteByAdmin); //deletes selected user, only for admin //+
