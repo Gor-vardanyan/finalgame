e148bde67abbaf4545ed4837ce2a54bd947f2a9a
Gor <g.var.design@gmail.com>
2021-01-01 02:47:34 +0100

    connection

--- controllers/PlayersController.js
+++ controllers/PlayersController.js
@@ -46,7 +46,7 @@ const showPlayersUser = async (req, res) => {
         console.log(player_user)
         const all = player_user.players;
         let play = []
-        all.forEach(element => { play.push( await PlayersModel.find({name:element}));}); 
+        await all.forEach(element => { play.push( await PlayersModel.find({name:element}));}); 
         res.send(play)
 } catch (error) {console.log(error)}
 };
